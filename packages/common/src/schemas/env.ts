572eec658dbbd5bdbf2fc3e17f2ed1bb068a7861
zeidkazi <zeidkazi7860@gmail.com>
2026-01-26 13:56:46 +0530

    [env] update

--- packages/common/src/schemas/env.ts
+++ packages/common/src/schemas/env.ts
@@ -8,6 +8,18 @@ config({
 const envSchema = z.object({
   PORT: z.string(),
   DATABASE_URL: z.string(),
+
+  FRONTEND_URL: z.string(),
+  BACKEND_URL: z.string(),
+
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.url(),
+
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
   // ... more
 });
 
