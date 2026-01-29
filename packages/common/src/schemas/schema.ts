acc73a02c5bfdbd424ed25b8f1bc0cf3bc24b18e
zeidkazi <zeidkazi7860@gmail.com>
2026-01-29 22:47:32 +0530

    [zod] validation schema for createFeedback

--- packages/common/src/schemas/schema.ts
+++ packages/common/src/schemas/schema.ts
@@ -2,7 +2,19 @@
 
 import z from "zod";
 
-export const DummySchema = z.object({
-  name: z.string(),
-  // ... more
+export const createFeedbackSchema = z.object({
+  domainId: z.string(),
+  url: z.string(),
+  screenshotUrl: z.string(),
+  message: z.string().optional(),
+  email: z.email(),
+
+  clientContext: z
+    .record(z.union([z.string(), z.number(), z.symbol()]), z.any())
+    .optional(),
+  debugContext: z
+    .record(z.union([z.string(), z.number(), z.symbol()]), z.any())
+    .optional(),
 });
+
+export type TcreateFeedbackPayload = z.infer<typeof createFeedbackSchema>;
