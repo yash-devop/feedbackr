2a3bbab28ced63a72332bccc46cc1c2ffaa58115
yash-devop <yashkamble.dev@gmail.com>
2026-02-03 17:23:10 +0530

    [remove] unwanted imports

--- apps/server/src/modules/feedback/feedback.route.ts
+++ apps/server/src/modules/feedback/feedback.route.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { FeedBackController } from "./feedback.controller.js";
-import { responseMiddleware } from "@/middlewares/response.middleware.js";
 
 export const FeedbackRouter = Router();
 
