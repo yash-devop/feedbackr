50e063150d64b13025c2e4e94feaaca666fb5bfd
yash-devop <yashkamble.dev@gmail.com>
2026-02-03 15:44:41 +0530

    [middleware] domain whitelisting

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,27 +1,27 @@
+import { env } from "@repo/common/env";
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import express from "express";
 import { corsConfig } from "./config/config.js";
-import { authMiddleware } from "./middlewares/auth.middleware.js";
-import { FeedbackRouter } from "./modules/feedback/feedback.route.js";
-import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/better-auth/auth.js";
-import { env } from "@repo/common/env";
-import { responseMiddleware } from "./middlewares/response.middleware.js";
+import { authMiddleware } from "./middlewares/auth.middleware.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
+import { responseMiddleware } from "./middlewares/response.middleware.js";
+import { FeedbackRouter } from "./modules/feedback/feedback.route.js";
+import { UserDomainRouter } from "./modules/user-domain/userDomain.route.js";
 
 export const app = express();
 
 app.use(cors(corsConfig));
-
 app.use(express.json());
 
 app.use(responseMiddleware);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/api/feedback", authMiddleware, FeedbackRouter);
+app.use("/api/domain", authMiddleware, UserDomainRouter);
 
 app.use(errorMiddleware);
-
 app.listen(process.env.PORT, async () => {
   console.log(`Server started successfully on PORT ${env.PORT}`);
 });
---
+++ apps/server/src/middlewares/domain.middleware.ts
@@ -0,0 +1,45 @@
+import { prisma } from "@/lib/prisma-orm/prisma.js";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./error.middleware.js";
+import { hashFunction } from "@repo/utils";
+export const domainMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const clientId = req.headers["x-client-id"] as string;
+
+  if (!clientId) {
+    throw new AppError("Missing Client ID", 400, "NOT_FOUND");
+  }
+
+  const isLocalhost = req.hostname.includes("localhost"); // http://localhost:<port>
+  if (isLocalhost) {
+    next();
+    return;
+  }
+
+  const hashedclientId = hashFunction(clientId);
+  const origin = req.headers.origin || req.headers.referer;
+
+  if (!origin) {
+    throw new AppError("Origin not found", 404);
+  }
+  const hostname = new URL(origin).hostname;
+
+  const domain = await prisma.domain.findFirst({
+    where: { clientId: hashedclientId, url: hostname },
+  });
+
+  if (!domain) {
+    throw new AppError("Invalid Client ID", 400);
+  }
+
+  next();
+};
+
+/**
+ * Remaining:
+ * 1) Cors level conditions.
+ * 2) block http requests.. keep https only.
+ */
