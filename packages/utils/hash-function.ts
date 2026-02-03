ca210b1a2261f8c6d3ac7dc058aa8f32cc14ef97
yash-devop <yashkamble.dev@gmail.com>
2026-02-03 15:43:38 +0530

    [utility] hash function to generate hash using crypto module

--- packages/utils/hash-function.ts
+++ packages/utils/hash-function.ts
@@ -1,5 +1,5 @@
 import { createHmac } from "crypto";
-export const generateHash = (data: string) => {
+export const hashFunction = (data: string) => {
   const SECRET = "feedbackr_e321hu3hb2yu1b";
   return createHmac("sha256", SECRET).update(data).digest("base64url"); // digest is the final fixed length string
 };
