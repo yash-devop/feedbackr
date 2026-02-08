7208112f280ff1ec545fd4740bc48ca8d25cfdbe
zeidkazi <zeidkazi7860@gmail.com>
2026-02-08 18:49:11 +0530

    [shadcn] fix import issue

--- apps/web/src/router/RouterBase.tsx
+++ apps/web/src/router/RouterBase.tsx
@@ -1,4 +1,6 @@
+import DashboardLayout from "@/components/Layouts/DashboardLayout.tsx";
 import { PageLayout } from "@/components/Layouts/PageLayout.tsx";
+import MainPagesLayout from "@/components/MainPagesLayout.tsx";
 import { LoginPage } from "@/pages/LoginPage.tsx";
 import { createBrowserRouter } from "react-router";
 export const Router = createBrowserRouter([
@@ -10,6 +12,28 @@ export const Router = createBrowserRouter([
         // path: "/",
         index: true,
         element: <LoginPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard/1",
+            element: <MainPagesLayout />,
+            children: [
+              {
+                index: true,
+                element: (
+                  <p className="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem itaque nisi cupiditate fugit dignissimos unde ipsum
+                    doloribus, ad aliquid error!
+                  </p>
+                ),
+              },
+            ],
+          },
+        ],
       },
     ],
   },
