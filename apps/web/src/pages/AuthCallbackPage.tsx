50e0d98ca158e406d8a663097c9185365f6c588c
zeidkazi <zeidkazi7860@gmail.com>
2026-02-16 01:45:16 +0530

    feat: refactor AuthCallbackPage to use domain existence service and improve navigation logic

--- apps/web/src/pages/AuthCallbackPage.tsx
+++ apps/web/src/pages/AuthCallbackPage.tsx
@@ -1,43 +1,32 @@
 import { useAuth } from "@/features/auth/hooks.ts";
+import useGetDomainExistService from "@/services/getDomainExistService/useGetDomainExistService.ts";
 import { Spinner } from "@repo/ui";
 import { useEffect } from "react";
 import { Navigate, useLocation, useNavigate } from "react-router";
 
 export const AuthCallbackPage = () => {
-  //   const { userSession } = useAuth();
-  //   const location = useLocation();
-
-  //   if (location.pathname.startsWith("/auth") && userSession.data?.session) {
-  //     return <Navigate to={"/dashboard/home"} />;
-  //   }
-
   const navigate = useNavigate();
 
+  const {
+    data: { domains, hasDomains },
+    services: { getDomainExistService },
+  } = useGetDomainExistService();
+
   useEffect(() => {
-    const fetcher = async () => {
-      const response = await fetch("http://localhost:8001/api/domain/exists", {
-        method: "GET",
-        credentials: "include",
-      });
+    if (getDomainExistService?.isLoading) return;
+    if (hasDomains) {
+      const domainId = domains?.[0]?.id;
+      navigate(`/dashboard/${domainId}`);
+    } else {
+      navigate("/onboarding/create-domain");
+    }
+  }, [getDomainExistService, navigate]);
 
-      const data = await response.json();
-
-      console.log("DATA FRONTEND", data);
-
-      const hasDomains = data?.data?.hasDomains && data?.data?.length >= 0;
-      if (hasDomains) {
-        return navigate("/dashboard/home");
-      } else {
-        return navigate("/onboarding/create-domain");
-      }
-    };
-    fetcher();
-  }, []);
   return (
     <>
-      <div className="h-screen flex items-center justify-center gap-3 px-3 lg:px-0">
+      <div className="h-screen flex flex-col items-center justify-center gap-3 px-3 lg:px-0">
         <Spinner variant="primary" size="xs" />
-        <p className="text-neutral-500">Signing you in soon</p>
+        <p className="text-neutral-500">Setting up your workspace...</p>
       </div>
     </>
   );
