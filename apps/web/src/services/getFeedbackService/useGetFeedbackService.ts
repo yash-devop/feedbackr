8089ebb5c37f7d99fbe3fff955856caf086c6c58
zeidkazi <zeidkazi7860@gmail.com>
2026-02-15 21:05:17 +0530

    feat: add useGetFeedbackService and associated types for feedback retrieval

---
+++ apps/web/src/services/getFeedbackService/useGetFeedbackService.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { CACHE_KEYS } from "@repo/common/queryCacheKeys";
+import { API_URLS } from "@repo/common/apiUrls";
+import { handleGlobalGetRequestQuery } from "@/utils/httpFuntions.ts";
+import { IFeedbackResponse } from "./useGetFeedbackService.types.ts";
+
+const useGetFeedbackService = ({ domainId }: { domainId: string }) => {
+  const getFeedbackService = useQuery<IFeedbackResponse>({
+    queryKey: [CACHE_KEYS?.GET_FEEDBACKS, domainId],
+    queryFn: () => handleGlobalGetRequestQuery({ url: API_URLS?.GET_FEEBACKS }),
+  });
+
+  return {
+    services: { getFeedbackService },
+  };
+};
+
+export default useGetFeedbackService;
