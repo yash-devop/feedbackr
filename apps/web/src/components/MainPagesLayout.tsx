e814fbd874553b6fbda4451b7ff8062b34334d24
yash-devop <yashkamble.dev@gmail.com>
2026-02-16 13:00:40 +0530

    [style] modified the padding for the main layout

--- apps/web/src/components/MainPagesLayout.tsx
+++ apps/web/src/components/MainPagesLayout.tsx
@@ -4,11 +4,14 @@ const MainPagesLayout = ({ children }: { children: React.ReactNode }) => {
   const { open, isMobile } = useSidebar();
   console.log("isMobile", isMobile);
   return (
-    <div className="bg-white w-full h-full shadow-sm ring ring-neutral-300 rounded-sm p-2 overflow-y-auto">
-      {(!open || isMobile) && (
-        <SidebarTrigger className="bg-muted text-foreground! hover:bg-neutral-200! cursor-pointer" />
-      )}
-      {children}
+    <div className="bg-white w-full h-full shadow-sm ring ring-neutral-300 rounded-sm overflow-y-auto">
+      <div className="w-full border-b p-2 flex items-center gap-x-2">
+        {(!open || isMobile) && (
+          <SidebarTrigger className="bg-muted text-foreground! hover:bg-neutral-200! cursor-pointer" />
+        )}
+        Top
+      </div>
+      <main className="p-2">{children}</main>
     </div>
   );
 };
