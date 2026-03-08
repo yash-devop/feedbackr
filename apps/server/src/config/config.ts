57f949c220468be06d382eccf56322e0ba633ae2
yash-devop <yashkamble.dev@gmail.com>
2026-03-08 19:12:19 +0530

    callback method of cors

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -3,8 +3,8 @@ import { FRONTEND_URL } from "@repo/common/config";
 
 // cors config
 export const corsConfig: CorsOptions = {
-  // origin: FRONTEND_URL,
-  // origin: [FRONTEND_URL, "http://localhost:3001", "http://localhost:5174"], // "http://localhost:3001" is my dummy user website ( using serve )
-  origin: [FRONTEND_URL, "*"],
+  origin: (origin, callback) => {
+    callback(null, true); // reflect request origin
+  },
   credentials: true,
 };
